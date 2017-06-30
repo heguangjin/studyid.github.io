cd65b46c4fb2740576b86c849cd6d5a4973e4756
何广进 <1241@qq.com>
2017-06-30 14:37:02 +0800

    初步完成退出功能

--- views/static/js/common.js
+++ views/static/js/common.js
@@ -1,4 +1,5 @@
 define(["jquery", "template", "cookie"], function($, template){
+	// 侧边栏数据请求
 	$(function(){
 		//判断当前页是不是在登录页面，如果是就不做如下的操作
 		if("/dashboard/login" != location.pathname){
@@ -9,9 +10,21 @@ define(["jquery", "template", "cookie"], function($, template){
 	        //将模板渲染到页面中刚才挖坑的地方
 	        $("#userinfo").html(html);
 		} 
-
-		 
     })
+
+    // 头部退出按钮事件
+    $("#out").click(function(){
+	    $.ajax({
+	        url:"/api/logout",
+	        type:"post",
+	        success:function(data){
+	            console.log(data);
+	            if(data.code == 200){
+	               location.href ="/dashboard/login";
+	            }               
+	        }
+	    })  
+      })
 })
 
 
