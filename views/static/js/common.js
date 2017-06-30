9d78eaae3b3caef1b712fc68650e5092c63789ec
何广进 <1241@qq.com>
2017-06-30 21:20:13 +0800

    侧边栏功能全部完成了2017年6月30日21:20:08

--- views/static/js/common.js
+++ views/static/js/common.js
@@ -3,12 +3,19 @@ define(["jquery", "template", "cookie"], function($, template){
 	$(function(){
 		//判断当前页是不是在登录页面，如果是就不做如下的操作
 		if("/dashboard/login" != location.pathname){
-			//从cookie中获取登录成功后存储的用户信息
-	        var userInfo = JSON.parse($.cookie("userinfo"));
-	        var html = template("profile-tpl", userInfo);
+			//完善登录功能，当用户处于未登录状态的时候，需要让用户直接跳转到登录页面
+			//如果当前用户是出于登录状态cookie中就会有PHPSESSID这个属性否者就没有
+			if(!$.cookie("PHPSESSID")){
+				location.href = "/dashboard/login"
+			}else{
+				//从cookie中获取登录成功后存储的用户信息
+		        var userInfo = JSON.parse($.cookie("userinfo"));
+		        var html = template("profile-tpl", userInfo);
 
-	        //将模板渲染到页面中刚才挖坑的地方
-	        $("#userinfo").html(html);
+		        //将模板渲染到页面中刚才挖坑的地方
+		        $("#userinfo").html(html);
+			}
+
 		} 
     })
 
@@ -37,9 +44,19 @@ define(["jquery", "template", "cookie"], function($, template){
 	$(".navs a").each(function(i, v) {
     	if($(v).attr("href") == location.pathname){
         	$(v).addClass('active');
-        	// $(v).parent().parent().slideDown();
+        	$(v).parent().parent().slideDown();
    	 	}
 	});
+
+	//导航栏二级菜单显示功能
+	$(".navs>ul>li>ul").parent().click(function(){
+		console.log(9898);
+		$(this).children("ul").slideToggle();
+		// 当ul下面的任何一个li被选中时，ul本身就移除active类
+		if($(this).children("ul").find("a").hasClass('active')){
+			$(this).children("ul").removeClass('active');
+		}
+	})
 })
 
 
